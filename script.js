a9353cc69bad89200c674bcbe51b1057d840b639
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-07-29 17:34:42 +0100

    Add constructor for creating book objects

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const myLibrary = [];
 
 function Book() {
   // the constructor
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    let status;
+    if (this.read) {
+      status = "read";
+    } else {
+      status = "not read yet";
+    }
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${status}`;
+  };
 }
 
 function addBookToLibrary() {
