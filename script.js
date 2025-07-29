c625aa4651ad93724020aa2e964fbb3e738c72b1
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-07-29 17:38:57 +0100

    Add comments to the code

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+// Array that stores all books
 const myLibrary = [];
 
+// Constructor function that creates Books
 function Book() {
-  // the constructor
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+  this.id = crypto.randomUUID(); // Generates a unique ID for each book
+  this.title = title; // Sets the book's title
+  this.author = author; // Sets the book's author
+  this.pages = pages; // Sets the book's number of pages
+  this.read = read; // Sets the book's read status (true/false)
+
+  // Method that returns the book info as a string
   this.info = function () {
+    // Variable for book status
     let status;
+
+    // If statement to determine the read status of the book
     if (this.read) {
       status = "read";
     } else {
       status = "not read yet";
     }
+
+    // Returns a string with all the book info
     return `${this.title} by ${this.author}, ${this.pages} pages, ${status}`;
   };
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store in in the array
-}
+// Function to create a new book and add it to the library array
+function addBookToLibrary() {}
